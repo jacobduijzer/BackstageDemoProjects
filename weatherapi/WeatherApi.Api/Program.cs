b56c55ea4e7649c3c0d94e4be9d6b68d23584993
Jacob Duijzer <jacob@duijzer.com>
2022-09-12 08:48:11 +0200

    Added url to swagger

--- weatherapi/WeatherApi.Api/Program.cs
+++ weatherapi/WeatherApi.Api/Program.cs
@@ -1,10 +1,14 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-app.UseSwagger();
+app.UseSwagger(options => options.PreSerializeFilters.Add((swagger, req) =>
+    swagger.Servers = new List<OpenApiServer>{new OpenApiServer
+        {Url = "https://backstage-demo-weather-api.azurewebsites.net"}}));
 app.UseSwaggerUI();
 app.UseHttpsRedirection();
 app.UseAuthorization();
